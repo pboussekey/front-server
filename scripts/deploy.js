cb81a537deb08b8a79284db4913863c2aea36a53
gmasmejean <guillaume.masmejean@gmail.com>
2017-12-18 18:05:08 +0100

    Updating deploy script / rename / styles

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,18 +11,23 @@ if( haveToMinify ){
 }
 
 // MOVE FILE TO SRC ...
-fs.copyFileSync( rootPath+'/tmp/configuration.js', rootPath+'/src/configuration.js' );
-fs.copyFileSync( rootPath+'/tmp/index.html', rootPath+'/src/index.html' );
-fs.copyFileSync( rootPath+'/tmp/'+(haveToMinify?'main.min.css':'main.css'), rootPath+'/src/main.css' );
+copyFileSync( rootPath+'/tmp/configuration.js', rootPath+'/src/configuration.js' );
+copyFileSync( rootPath+'/tmp/index.html', rootPath+'/src/index.html' );
+copyFileSync( rootPath+'/tmp/'+(haveToMinify?'main.min.css':'main.css'), rootPath+'/src/main.css' );
 
 if( haveToMinify ){
-    fs.copyFileSync( rootPath+'/tmp/lib.min.js', rootPath+'/src/lib.js' );
-    fs.copyFileSync( rootPath+'/tmp/app.min.js', rootPath+'/src/app.js' );
-    fs.copyFileSync( rootPath+'/tmp/lib.min.js.map', rootPath+'/src/lib.js.map' );
-    fs.copyFileSync( rootPath+'/tmp/app.min.js.map', rootPath+'/src/app.js.map' );
+    copyFileSync( rootPath+'/tmp/lib.min.js', rootPath+'/src/lib.js' );
+    copyFileSync( rootPath+'/tmp/app.min.js', rootPath+'/src/app.js' );
+    copyFileSync( rootPath+'/tmp/lib.min.js.map', rootPath+'/src/lib.js.map' );
+    copyFileSync( rootPath+'/tmp/app.min.js.map', rootPath+'/src/app.js.map' );
 }else{
-    fs.copyFileSync( rootPath+'/tmp/lib.js', rootPath+'/src/lib.js' );
-    fs.copyFileSync( rootPath+'/tmp/app.js', rootPath+'/src/app.js' );
+    copyFileSync( rootPath+'/tmp/lib.js', rootPath+'/src/lib.js' );
+    copyFileSync( rootPath+'/tmp/app.js', rootPath+'/src/app.js' );
+}
+
+function copyFileSync( p1, p2 ){
+    let output = fs.openSync( p2,'w');
+    fs.appendFileSync( output, fs.readFileSync(p1) );
 }
 
 console.log('App files deployed...');
