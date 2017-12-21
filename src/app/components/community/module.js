2997a6e1fa599206f82db7250b42ae06071094e7
gmasmejean <guillaume.masmejean@gmail.com>
2017-12-21 15:52:26 +0100

    global search updates - placeholder / always visible

--- src/app/components/community/module.js
+++ src/app/components/community/module.js
@@ -1,27 +1,18 @@
 angular.module('community',['ui.router','API','EVENTS'])
-    .config(['$stateProvider', 
+    .config(['$stateProvider',
         function( $stateProvider ){
             $stateProvider.state('lms.community',{
                 url:'/community/:category',
                 controller:'community_controller as ctrl',
-                templateUrl:'app/components/community/tpl/main.html',
-                onEnter : ['global_search',function(global_search){
+                templateUrl:'app/components/community/tpl/main.html'
+                /*onEnter : ['global_search',function(global_search){
                     global_search.hide = true;
                 }],
                 onExit : ['global_search',function(global_search){
                     global_search.hide = false;
-                }]
-              
-
+                }]*/
             });
-        
-        }
-    ]).run([
-        function(){
-        
-        
-
         }
     ]);
-    
+
 ANGULAR_MODULES.push('community');
