c160ff1e47e7a17236c35349ae430ca7c9849fbb
gmasmejean <guillaume.masmejean@gmail.com>
2017-12-21 17:32:16 +0100

    User pages updates / Create+Discover buttons

--- src/app/components/userpages/controllers/user_pages.controller.js
+++ src/app/components/userpages/controllers/user_pages.controller.js
@@ -1,5 +1,6 @@
 angular.module('userpages').controller('userpages_controller',
-    ["pagetype", "user_pages_service","session",function( pagetype, user_pages_service, session ){
+    ['pagetype','user_pages_service','session','page_modal_service','oadmin_model','session',
+    function( pagetype, user_pages_service, session, page_modal_service, oadmin_model, session ){
 
         var ctrl = this,
             page = 1,
@@ -7,13 +8,22 @@ angular.module('userpages').controller('userpages_controller',
 
         ctrl.type = pagetype;
         ctrl.displayed_pages = [];
+        ctrl.canCreate = false;
 
         if( pagetype === 'event' ){
             ctrl.title = 'My events';
+            ctrl.linkCategory = 'events';
+            ctrl.canCreate = true;
         }else if ( pagetype === 'group' ) {
             ctrl.title = 'My groups';
+            ctrl.linkCategory = 'groups';
+            ctrl.canCreate = true;
         }else if ( pagetype === 'course' ) {
             ctrl.title = 'My courses';
+
+            oadmin_model.get([session.id]).then(function(){
+                ctrl.canCreate = session.roles[1] || oadmin_model.list[session.id].datum.length;
+            });
         }
 
         // SET TITLE
@@ -22,6 +32,10 @@ angular.module('userpages').controller('userpages_controller',
         user_pages_service.load([session.id],true).then(function(){
             ctrl.loadNextPages();
         });
+
+        ctrl.openPageModal = function($event){
+            page_modal_service.open( $event, ctrl.type );
+        };
 
         ctrl.loadNextPages = function(){
             Array.prototype.push.apply( ctrl.displayed_pages, user_pages_service.memberof.slice( n*(page-1), n*page) );
