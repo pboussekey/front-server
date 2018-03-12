4ce79f83cb548a77b62cb55664a25f77e505885a
pboussekey <pboussekey@thestudnet.com>
2018-03-12 09:33:32 +0100

    Increase infinite loading tolerance on dashboard posts + Labels on pages cta

--- src/app/shared/custom_elements/pages_actions/page_actions.controller.js
+++ src/app/shared/custom_elements/pages_actions/page_actions.controller.js
@@ -163,7 +163,7 @@ angular.module('customElements').controller('page_actions_controller',
                     template : 'app/shared/elements/confirm/modal.html',
                     reference : evt.target,
                     scope : {
-                        question : "Do you really want to leave this " + $scope.page.type + " ?",
+                        question : "Do you really want to leave this " + $scope.page.label + " ?",
                         confirm : function(){
                             $scope.leave(evt);
                         }
@@ -178,7 +178,7 @@ angular.module('customElements').controller('page_actions_controller',
                     template : 'app/shared/elements/confirm/modal.html',
                     reference : evt.target,
                     scope : {
-                        question : "Do you really want to delete this " + $scope.page.type + " ?",
+                        question : "Do you really want to delete this " + $scope.page.label + " ?",
                         confirm : function(){
                             $scope.delete(evt);
                         }
