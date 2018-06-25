6553261e3c87a91c9954e09f39d87cf8192a891a
pboussekey <pboussekey@thestudnet.com>
2018-06-25 09:23:06 +0200

    Notifications sentences

--- src/app/shared/notifications/services/notifications.service.js
+++ src/app/shared/notifications/services/notifications.service.js
@@ -1,7 +1,7 @@
 angular.module('notifications_module')
     .factory('notifications_service',['filters_functions', 'pages_config',
-        function(filters_functions, pages_config){            
-         
+        function(filters_functions, pages_config){
+
             var service = {
                 post_update_types:['post.create', 'post.update', 'post.com', 'post.like', 'post.tag',
                     'page.member', 'connection.accept','connection.request', 'page.invited'],
@@ -12,16 +12,22 @@ angular.module('notifications_module')
                     /*"user.update": function(notification){
                         return filters_functions.username(notification.source.data, true) + " has an updated profile";
                     },*/
-                    "post.create": function(notification){ 
+                    "post.create": function(notification){
                         return filters_functions.username(notification.source.data, true) + " <b>published</b> a post";
                     },
-                    "post.com": function(notification){ 
+                    "post.com": function(notification){
                         return filters_functions.username(notification.source.data, true) + " <b>commented</b> on a post";
                     },
                     "page.member":
                     function(notification){
                         var label = pages_config[notification.object.data.page.type].label;
-                        return filters_functions.username(notification.source.data, true) + " <b>enrolled</b> you in a new " + label;
+                        if(['course', 'organization'].indexOf(notification.object.data.page.type) !== -1){
+                          return filters_functions.username(notification.source.data, true) + " <b>enrolled</b> you in a new " + label;
+                        }
+                        else{
+
+                            return filters_functions.username(notification.source.data, true) + " <b>joined</b> a new " + label;
+                        }
                     },
                     "page.invited":
                     function(notification){
@@ -34,15 +40,15 @@ angular.module('notifications_module')
                         return filters_functions.username(notification.source.data, true) + " <b>requested</b> to join your " + label;
                     },
                     "post.like":
-                    function(notification){ 
+                    function(notification){
                         return filters_functions.username(notification.source.data, true) + " <b>liked</b> a post";
                     },
                     "post.tag":
-                    function(notification){ 
+                    function(notification){
                         return filters_functions.username(notification.source.data, true) + " <b>mentionned</b> you in a post";
                     }
                 },
-          
+
                 clearEvents : function(){
                     service.list = [];
                     service.unread_notifications = 0;
