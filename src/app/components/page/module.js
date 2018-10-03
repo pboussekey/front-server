7f6d377408e95e050f22c625241af7e2732ca858
pboussekey <pboussekey@thestudnet.com>
2018-10-03 21:39:13 +0200

    Conversation remove loader

--- src/app/components/page/module.js
+++ src/app/components/page/module.js
@@ -23,73 +23,62 @@ angular.module('page',['ui.router','API','EVENTS'])
                 url : "/timeline",
                 templateUrl: '/app/components/page/tpl/timeline.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             })
             .state("lms.page.users", {
                 templateUrl: '/app/components/page/tpl/users.html',
                 controller: 'page_users_controller as ctrl',
                 redirectTo : 'lms.page.users.all',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             })
             .state("lms.page.users.all", {
                 url : "/everyone",
                 templateUrl: '/app/components/page/tpl/all.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             })
             .state("lms.page.users.admin", {
                 url : "/administrators",
                 templateUrl: '/app/components/page/tpl/administrators.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state("lms.page.users.attendees", {
                 url : "/attendees",
                 templateUrl: '/app/components/page/tpl/attendees.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state("lms.page.users.alumni", {
                 url : "/alumni",
                 templateUrl: '/app/components/page/tpl/alumni.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state('lms.page.users.community', {
                 url : "/community",
                 templateUrl: '/app/components/page/tpl/community.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state("lms.page.events", {
                 url : "/events",
                 templateUrl: '/app/components/page/tpl/events.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state("lms.page.resources", {
                 url : "/resources",
                 templateUrl: '/app/components/page/tpl/resources.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state('lms.page.relationship', {
                 url : "/relationship",
                 templateUrl: '/app/components/page/tpl/relationship.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state('lms.page.content', {
                 url : "/content/:item_id",
                 templateUrl: '/app/components/page/tpl/course_content.html',
                 controller: 'course_content_controller as ctrl',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state('lms.page.grades', {
                 url : "/grades",
                 templateUrl: '/app/components/page/tpl/organization_grades.html',
@@ -102,21 +91,18 @@ angular.module('page',['ui.router','API','EVENTS'])
                             return orggrades_model.list[$stateParams.id].datum;
                         });
                     }]
-                },
-                global_loading : ['ctrl_loaded']
+                }
             }).state('lms.page.analytics', {
                 url : "/analytics",
                 templateUrl: '/app/components/page/tpl/analytics.html',
                 controller: 'organization_analytics_controller as ctrl',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             }).state('lms.page.custom', {
                 url : "/custom",
                 templateUrl: '/app/components/page/tpl/custom.html',
                 nested : 'lms.page',
-                parent_state : 'lms.community',
-                global_loading : ['ctrl_loaded']
+                parent_state : 'lms.community'
             });
         }
     ]).run([
