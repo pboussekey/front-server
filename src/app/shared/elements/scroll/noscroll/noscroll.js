ccb98a06f203fc16e015d0011538d5a7e0ed3cfa
pboussekey <pboussekey@thestudnet.com>
2018-11-03 19:39:25 +0100

    Simplify noscroll directive

--- src/app/shared/elements/scroll/noscroll/noscroll.js
+++ src/app/shared/elements/scroll/noscroll/noscroll.js
@@ -8,30 +8,14 @@ angular.module('elements')
             link : function(scope, element){
 
                 // BIND EVENTS
-                element[0].addEventListener('mouseover', function(e){
-                    if (!document.querySelector('#body').classList.contains('noscroll')) {
-                        var containerHeight = element[0].outerHeight;
-                        var contentHeight = element[0].scrollHeight;
-
-                        if (contentHeight>containerHeight || scope.force){
-                            document.querySelector('#body').classList.add('noscroll');
-                        }
-                    }
-                  }, true);
-
-                element[0].addEventListener('mouseout', function(){
-                      document.querySelector('#body').classList.remove('noscroll');
-                }, true);
-
-
                 element[0].addEventListener('wheel', function(e){
-                    if (!document.querySelector('#body').classList.contains('noscroll')) {
-                           return;
+                    var containerHeight = element[0].outerHeight;
+                    var contentHeight = element[0].scrollHeight;
 
-                     } else {
+                    if (contentHeight>containerHeight || scope.force){
                          e.preventDefault();
                          return false;
-                     }
+                   }
                 }, true);
 
             }
