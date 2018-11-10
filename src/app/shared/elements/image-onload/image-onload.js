0dd0d7014e3a1f5fb377796015793cf1f973a562
pboussekey <pboussekey@thestudnet.com>
2018-11-10 01:25:17 +0100

    Profile pictures remove original size loading

--- src/app/shared/elements/image-onload/image-onload.js
+++ src/app/shared/elements/image-onload/image-onload.js
@@ -24,6 +24,7 @@ angular.module('elements')
 
                             falseimg.onload = function(){
                               if(!element[0].classList.contains('loaded')){
+                                  console.log("FALSE IMAGE LOADED", falseurl);
                                   element[0].style.backgroundImage = "url('"+ falseurl + "')";
                                   element[0].classList.add("preloaded");
                               }
@@ -34,6 +35,7 @@ angular.module('elements')
                         img.onerror = function(){
                             if(!original){
                                 url = filters_functions.dmsLink(scope.url);
+                                console.log("SET ORIGINAL", url);
                                 original = true;
                             }
                             element[0].classList.add("loaded");
@@ -41,9 +43,11 @@ angular.module('elements')
                             if(scope.onerror){
                                 scope.onerror();
                             }
+                            console.log("ON ERROR", url);
                             element[0].style.backgroundImage = "url('"+ url + "')";
                         };
                         img.onload = function () {
+                            console.log("IMAGE LOADED", url);
                            element[0].style.backgroundImage = "url('"+ url + "')";
                            element[0].classList.add("loaded");
                         };
